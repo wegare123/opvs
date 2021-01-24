8e31b935ac2d0eecbc90e5e47b05bbf55677375e
wegare123 <56117745+wegare123@users.noreply.github.com>
2021-01-24 07:29:00 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #opvs (Wegare)
-wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/openvpn-stunnel/main/opvs.sh" -O /usr/bin/opvs
-wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/openvpn-stunnel/main/autorekonek-opvs.sh" -O /usr/bin/autorekonek-opvs
+wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/opvs/main/opvs.sh" -O /usr/bin/opvs
+wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/opvs/main/autorekonek-opvs.sh" -O /usr/bin/autorekonek-opvs
 opkg update && opkg install lsof openvpn-openssl stunnel
 chmod +x /usr/bin/opvs
 chmod +x /usr/bin/autorekonek-opvs
