4a05263a790da0a812d3f54acd826ac81ebe61f0
wegare123 <56117745+wegare123@users.noreply.github.com>
2021-03-14 22:09:50 +0700

    Add files via upload

--- autorekonek-opvs.sh
+++ autorekonek-opvs.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #opvs (Wegare)
-host="$(cat /root/akun/opvs.txt | grep -i host | cut -d= -f2 | head -n1)"
-route="$(route | grep -i $host| head -n1 | awk '{print $1}')" 
+route="$(lsof -i | grep -i stunnel | grep -i listen)" 
 route2="$(route | grep -i tun0 | head -n1 | awk '{print $8}')" 
 	if [[ -z $route2 ]]; then
 		   (printf '3\n'; sleep 15; printf '\n') | opvs	
